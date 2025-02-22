8279a15fb0810d130707158000f853fdd281fc60
Roberto <robertoz1777@gmail.com>
2025-02-22 17:24:16 +0100

    Primer commit: Subiendo el proyecto Wasaasp

---
+++ servidor/server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+const hostname = 'localhost';
+const { Server } = require('socket.io');
+const { createServer } = require('node:http');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+var numUsuarios = 0;
+const usuarios = new Map();
+
+// Reemplazar la ruta raíz para que verifique la identificación
+app.get('/', (req, res) => {
+  if (!req.query.nombre) {
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+  } else {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  numUsuarios++;
+  console.log('Nuevo usuario. Tengo ' + numUsuarios + ' usuarios conectados.');
+
+  socket.on("nombre", (nombre) => {
+    socket.nombreUsuario = nombre; // Guardamos el nombre en el socket
+    console.log("Se ha conectado = " + nombre);
+    io.emit("nombre", nombre);
+  });
+
+  socket.on("identificacion", (userData) => {
+    // userData = { nombre, estado, avatar }
+    socket.userData = userData;
+    usuarios.set(socket.id, userData);
+    io.emit("nuevoUsuario", [...usuarios.values()]);
+  });
+
+  socket.on("escribiendo", () => {
+    io.emit("usuarioEscribiendo", socket.userData?.nombre);
+  });
+
+  socket.on("dejoDeEscribir", () => {
+    io.emit("usuarioDejoDeEscribir", socket.userData?.nombre);
+  });
+
+  socket.on('mensaje', (datos) => {
+    console.log("Soy el servidor y he recibido un mensaje con datos = " + datos);
+    io.emit("holaDesdeElServidor", datos);
+  });
+
+  socket.on('disconnect', () => {
+    usuarios.delete(socket.id);
+    io.emit("seHaDesconectado", socket.userData?.nombre);
+    numUsuarios--;
+    console.log("Ahora hay " + numUsuarios + " usuarios conectados.");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Run server on http://${hostname}:${port}`);
+}).on('error', (err) => {
+  console.error("Server error: ", err);
+});
